80776a8a9e434c911cdf229748a01c4c6dc40689
Daniel Minschinski <danielminschinski@gmail.com>
2019-12-21 11:41:51 +0100

    install react router and add styles

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/global.scss';
+import 'animate.css/animate.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 const Root = () => (
   <App />
