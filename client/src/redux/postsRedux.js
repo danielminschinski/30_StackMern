1747342ff72aacaf511fd16ee5ca37a6c86596fc
Daniel <danielminschinski@gmail.com>
2020-01-03 08:16:00 +0100

    modified postsRedux

--- client/src/redux/postsRedux.js
+++ client/src/redux/postsRedux.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { API_URL } from '../config';
 
 //SELECTORS
-export const getPosts = ({ posts }) => posts;
-export const countPosts = ({ posts }) => posts.length;
+export const getPosts = ({ posts }) => posts.data;
+export const countPosts = ({ posts }) => posts.data.length;
 
 //action name creator
 const reducerName = 'posts';
@@ -12,19 +12,33 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 //ACTIONS
 export const LOAD_POSTS = createActionName('LOAD_POSTS');
+export const START_REQUEST = createActionName('START_REQUEST');
+export const END_REQUEST = createActionName('END_REQUEST');
+
 export const loadPosts = payload => ({ payload, type: LOAD_POSTS });
+export const startRequest = () => ({ type: START_REQUEST });
+export const endRequest = () => ({ type: END_REQUEST});
 
 
 // initial state
 
-const initialState = [];
+const initialState = {
+    data: [],
+    request: {
+        pending: false,
+    },
+};
 
 // reducer
 
 export default function reducer(statePart = initialState, action = {}){
     switch(action.type){
         case LOAD_POSTS:
-            return [ ...action.payload ];
+            return { ...statePart, data: action.payload };
+        case START_REQUEST:
+            return { ...statePart, request: { pending: true } };
+        case END_REQUEST:
+            return { ...statePart, request: { pending: false } };
         default:
             return statePart;
     }
