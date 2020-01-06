4af9f2c5028fd1a5ce665d654abfa66e83b74659
Daniel Minschinski <danielminschinski@gmail.com>
2020-01-06 21:35:25 +0100

    modified index.js config

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const PostController = require('../controllers/post.controller');
 
 // get all posts
-router.route('./posts').get(PostController.getPosts);
+router.route('/posts').get(PostController.getPosts);
 
 module.exports = router;
