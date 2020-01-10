d1320b8b95f3681126c967423f5803349f550dbd
Daniel Minschinski <danielminschinski@gmail.com>
2020-01-10 20:27:05 +0100

    modified posts single post

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
 import './styles/global.scss';
 import 'animate.css/animate.min.css';
+
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
