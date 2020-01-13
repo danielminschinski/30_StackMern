ecf88359a37041910ef05ba482547f92a8829824
Daniel <danielminschinski@gmail.com>
2020-01-13 13:48:09 +0100

    modyfied post.controller post.router

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post.model');
+const uuid = require('uuid');
 
 // get all posts
 
@@ -16,6 +17,23 @@ exports.getPosts = async (req, res) => {
 exports.getSinglePost = async (req, res) => {
   try {
     res.status(200).json(await Post.findOne( {id: req.params.id }));
+  } catch(err){
+    res.status(500).json(err);
+  }
+};
+
+// add new post
+
+exports.addPost = async function (req, res){
+  try{
+    const { title, author, content } = req.body;
+
+    let newPost = new Post(req.body);
+    newPost.id = uuid();
+
+    postSaved = await newPost.save();
+    res.status(200).json(postSaved);
+
   } catch(err){
     res.status(500).json(err);
   }
