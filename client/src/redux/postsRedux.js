6904cd9e4ac0cc7324c8b0d7b34b6eb4bd371942
Daniel Minschinski <danielminschinski@gmail.com>
2020-01-15 21:30:16 +0100

    modified postsRedux and postFormContainer

--- client/src/redux/postsRedux.js
+++ client/src/redux/postsRedux.js
@@ -93,3 +93,17 @@ export const loadPostsRequest = () => {
         }
       };
   };
+
+  export const addPostRequest = (post) => {
+      return async dispatch => {
+
+        dispatch(startRequest());
+        try{
+            let res = await axios.post(`${API_URL}/posts`, post);
+            await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+            dispatch(endRequest());
+        }catch(e){
+            dispatch(errorRequest(e.message));
+        }
+      }
+  }
