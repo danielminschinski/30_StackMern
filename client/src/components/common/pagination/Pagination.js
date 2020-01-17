0949fcc3488377413db383d7ed30a9afa938ba85
Daniel <danielminschinski@gmail.com>
2020-01-17 16:50:39 +0100

    modyfied pagination posts postredux

--- client/src/components/common/pagination/Pagination.js
+++ client/src/components/common/pagination/Pagination.js
@@ -7,17 +7,35 @@ class Pagination extends React.Component {
     state = {
         presentPage: this.props.initialPage || 1
     }
+
+    changePage = (newPage) => {
+        const { onPageChange } = this.props;
+
+        this.setState({ presentPage: newPage });
+        onPageChange(newPage);
+    }
     
     
     
     render(){
 
         const { pages, onPageChange } = this.props;
-        const { presentPage } = this.props;
+        const { presentPage } = this.state;
         const { changePage } = this;
         return (
             <div className="pagination">
+                <ul className="pagination__list">
 
+                    {[...Array(pages)].map((el, page) =>
+                    <li
+                        key={++page}
+                        onClick={() => {changePage(page) }}
+                        className={`pagination__list__item${((page) === presentPage) ? ' pagination__list__item--active' : ''}`}>
+                        {page}             
+                    </li>
+                    )}
+
+                </ul>
             </div>
         );
     }
