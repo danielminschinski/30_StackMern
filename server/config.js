1c57174bca016c5e577a959a57cc9c14f5f07372
Daniel <danielminschinski@gmail.com>
2020-01-22 13:27:23 +0100

    modified config server

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: 8000,
-    DB: 'mongodb://localhost:27017/mernapp',
+    DB: 'mongodb+srv://blackmin:<michnik1990>@cluster0-coujl.mongodb.net/test?retryWrites=true&w=majority',
   };
