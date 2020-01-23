2b844eb84d5ec75ed0773288343ced2c115e46b9
Daniel <danielminschinski@gmail.com>
2020-01-23 08:05:53 +0100

    modified config server

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  PORT: process.env.PORT || 8000,
-  DB: process.env.DB,
-};
+    PORT: 8000,
+    DB: 'mongodb+srv://blackmin:<michnik1990>@cluster0-coujl.mongodb.net/test?retryWrites=true&w=majority',
+  };
